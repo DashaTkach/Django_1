2530891e0dcc28835804cde77ef6355a4ea5df67
DashaTkach <120254382+DashaTkach@users.noreply.github.com>
2023-08-31 21:53:25 +0300

    Update views.py

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -7,11 +7,24 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+        list_of_names = []
+    list_of_prices = []
+    for phone in phones:
+        list_of_names.append(phone.name)
+        list_of_prices.append(phone.price)
+    context = {
+        'phones': phones,
+        'name': sorted(list_of_names),
+        'max_price': sorted(list_of_prices, reverse=True),
+        'min_price': sorted(list_of_prices),
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {
+        'phones': phones,
+        'slug': slug
+    }
     return render(request, template, context)
