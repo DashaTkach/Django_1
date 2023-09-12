b8ad3c1f100786235d095cd5080f47dc01d7eba9
DashaTkach <120254382+DashaTkach@users.noreply.github.com>
2023-09-12 16:49:56 +0300

    Update models.py

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Measurement(models.Model):
+    temperature = models.BooleanField()
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Sensor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.SlugField()
+    measurements = models.ForeignKey(Measurement, on_delete=models.CASCADE)
